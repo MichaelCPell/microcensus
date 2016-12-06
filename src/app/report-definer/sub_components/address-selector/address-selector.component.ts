36b1e1ae0f213a33c48cf440f03690935286e86d
Michael Pell <MichaelCPell@gmail.com>
2016-12-06 15:39:10 -0500

    Adds applicationRef.tick

--- src/app/report-definer/sub_components/address-selector/address-selector.component.ts
+++ src/app/report-definer/sub_components/address-selector/address-selector.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ApplicationRef } from '@angular/core';
 import { ResearchAreaService } from '../../../shared/research-area.service'
 
-declare var google:any;
+
 
 @Component({
   selector: 'app-address-selector',
@@ -12,19 +12,25 @@ export class AddressSelectorComponent implements OnInit {
   // public map;
 
 
-  constructor(private researchArea: ResearchAreaService) {
+  constructor(private researchArea: ResearchAreaService, private af: ApplicationRef) {
     this.researchArea = researchArea
   }
 
   ngOnInit() {
     let element:any = document.getElementById("address")
     let autocomplete = new google.maps.places.Autocomplete(element);
+
+
+    console.log(google)
+    // let autocomplete = new g
+
     autocomplete.addListener('place_changed', () => {
       var place = autocomplete.getPlace(autocomplete);
       var latLng = [place.geometry.location.lat(), place.geometry.location.lng()];
 
       console.log("Flag One")
       this.researchArea.zebra = "Chinchilla";
+      this.af.tick();
       // this.map.setCurrentAddress(place.formatted_address  + ", 1 Mile Radius")
       // this.map.addMarker(latLng);
     });
