adeb29ff4e2a7446e36bf56c643874c084c0ae7e
Michael Pell <MichaelCPell@gmail.com>
2016-12-13 19:57:21 -0500

    User form sort of in place

--- src/app/users/session-dashboard/session-dashboard.component.ts
+++ src/app/users/session-dashboard/session-dashboard.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session-dashboard.component.css']
 })
 export class SessionDashboardComponent implements OnInit {
-  public newUser:string = "Andrea";
 
 
   constructor() { }
@@ -15,41 +14,6 @@ export class SessionDashboardComponent implements OnInit {
   }
 
 
-  public createUser(){
-    console.log(this.newUser.name);
-  }
 
-
-  public createUserX(){
-    console.log("Full Steam Ahead")
-    var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
-    var poolData = {
-      UserPoolId : 'us-east-1_T2p3nd9xA', // Your user pool id here
-      ClientId : '58qe0b7458eo9705kijc7hjhv6' // Your client id here
-    };
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
-    var attributeList = [];
-
-    var dataEmail = {
-      Name : 'email',
-      Value : 'email@mydomain.com'
-    };
-
-    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-
-    attributeList.push(attributeEmail);
-
-    userPool.signUp('username2', 'password', attributeList, null, function(err, result){
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log(result)
-      cognitoUser = result.user;
-      console.log('user name is ' + cognitoUser.getUsername());
-    });
-
-  }
 
 }
--- src/app/users/user-registration/user-registration.component.ts
+++ src/app/users/user-registration/user-registration.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../user";
+import { FormsModule }   from '@angular/forms';
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const poolData = {
+  UserPoolId : 'us-east-1_T2p3nd9xA',
+  ClientId : '58qe0b7458eo9705kijc7hjhv6'
+};
 
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
-  styleUrls: ['./user-registration.component.css']
+  styleUrls: ['./user-registration.component.css'],
+  imports: [FormsModule]
 })
 export class UserRegistrationComponent implements OnInit {
+
+  newUser = new User("", "");
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public onSubmit(){
+    console.log("onSubmit()")
+    this.foo = new User(this.newUser.email, this.newUser.password)
+  }
 }
