5fd82c996665511bb70cc9bb17f2d5ca39bd0f10
Michael Pell <MichaelCPell@gmail.com>
2016-12-14 11:14:59 -0500

    Working User Registration Flow

--- src/app/users/user-registration/user-registration.component.ts
+++ src/app/users/user-registration/user-registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../user";
 import { FormsModule }   from '@angular/forms';
+import {Subscriber} from "rxjs/Subscriber";
+import {Router} from "@angular/router";
+
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 const poolData = {
   UserPoolId : 'us-east-1_T2p3nd9xA',
@@ -10,20 +13,55 @@ const poolData = {
 @Component({
   selector: 'app-user-registration',
   templateUrl: './user-registration.component.html',
-  styleUrls: ['./user-registration.component.css'],
-  imports: [FormsModule]
+  styleUrls: ['./user-registration.component.css']
 })
 export class UserRegistrationComponent implements OnInit {
 
   newUser = new User("", "");
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+
+  }
+
+  runExperiment(){
+    this.verifyUser()
   }
 
   public onSubmit(){
     console.log("onSubmit()")
-    this.foo = new User(this.newUser.email, this.newUser.password)
+    this.newUser = new User(this.newUser.email, this.newUser.password);
+    this.newUser.create().subscribe(
+      (next) => {
+        console.log(next);
+        this.verifyUser()
+      },
+      (error) => {
+        switch(error.code){
+          case "UsernameExistsException":
+          break;
+          default:
+            console.log("Uncaught Error Code: %s", error.code)
+          break;
+        }
+      },
+      () => console.log('onCompleted')
+    );
+  }
+
+
+
+  public verifyUser(){
+    let code = prompt("Please Enter Your Verification Code (Check Your Email)");
+    this.newUser.verify(code).subscribe(
+      (next) => {
+        console.log("User Successfully Verified")
+        this.router.navigate(["/dashboard"]);
+      },
+      (error) => {
+
+      }
+    )
   }
 }
--- src/app/users/user.ts
+++ src/app/users/user.ts
@@ -4,12 +4,14 @@ var poolData = {
   ClientId : '58qe0b7458eo9705kijc7hjhv6' // Your client id here
 };
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
+import {Observable} from 'rxjs/Observable';
 
 export class User {
 
 
-  constructor(public email:string, private password:string, private needsRegistration?:boolean){
+  constructor(public email:string, public password:string, private needsRegistration?:boolean){}
+
+  public create(){
     var attributeList = [];
 
     var dataEmail = {
@@ -17,55 +19,74 @@ export class User {
       Value : this.email
     };
 
+    return Observable.create( (observer) => {
+      var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
 
-    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+      attributeList.push(attributeEmail);
 
-    attributeList.push(attributeEmail);
-
-    //
-    if(this.needsRegistration){
       userPool.signUp(this.email, this.password, attributeList, null, function(err, result){
         if (err) {
-          console.log(err);
+          observer.error(err);
           return;
         }
-        console.log(result);
+        observer.next(result);
       });
-    }else{
-      var authenticationData = {
-             Username : this.email,
-             Password : this.password
-       };
-      var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    })
+  }
+  // public authenticate(){
+  //   return new Promise((resolve, error) => {
+  //     var authenticationData = {
+  //            Username : this.email,
+  //            Password : this.password
+  //      };
+  //     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+  //
+  //     var userData = {
+  //         Username : this.email,
+  //         Pool : userPool
+  //     };
+  //     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  //     cognitoUser.authenticateUser(authenticationDetails, {
+  //       onSuccess: function (result) {
+  //           console.log('access token + ' + result.getAccessToken().getJwtToken());
+  //
+  //           // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  //           //     IdentityPoolId : '...', // your identity pool id here
+  //           //     Logins : {
+  //           //         // Change the key below according to the specific region your user pool is in.
+  //           //         'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : result.getIdToken().getJwtToken()
+  //           //     }
+  //           // });
+  //
+  //           // Instantiate aws sdk service objects now that the credentials have been updated.
+  //           // example: var s3 = new AWS.S3();
+  //       },
+  //       onFailure: function(err) {
+  //         console.log(err)
+  //       }
+  //     })
+  //   })
+  // }
 
+
+  public verify(code){
       var userData = {
           Username : this.email,
           Pool : userPool
       };
       var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-      cognitoUser.authenticateUser(authenticationDetails, {
-          onSuccess: function (result) {
-              console.log('access token + ' + result.getAccessToken().getJwtToken());
 
-              // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-              //     IdentityPoolId : '...', // your identity pool id here
-              //     Logins : {
-              //         // Change the key below according to the specific region your user pool is in.
-              //         'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : result.getIdToken().getJwtToken()
-              //     }
-              // });
 
-              // Instantiate aws sdk service objects now that the credentials have been updated.
-              // example: var s3 = new AWS.S3();
+      return Observable.create( (observer) => {
+        cognitoUser.confirmRegistration(code, false, function(err, msg){
+          if(err){
+            console.log("Error" + err)
+            return
+          }
 
-          },
+          observer.next(msg)
+        })
+      })
 
-          onFailure: function(err) {
-            console.log(err)
-              alert(err);
-          },
-
-      });
-    }
   }
 }
