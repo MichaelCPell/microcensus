d20902d7a5a909a445627df101894835798e5282
Michael Pell <MichaelCPell@gmail.com>
2017-01-24 19:01:06 -0500

    Updates poverty numbers

--- src/report_templates/grid_poverty.js
+++ src/report_templates/grid_poverty.js
@@ -8,17 +8,16 @@ document.getElementById("total-families-poverty").innerHTML = data["Total Famili
 
 
 
-document.getElementById("two-parent-poverty-with-schoolchildren").innerHTML = data["Married Family - Children under 18"] - data["Married Family - Children under 18 - under 5"];
+
 
 
 var totalFamiliesWithSchoolchildren =
-  (data["Married Family - Children under 18"] - data["Married Family - Children under 18 - under 5"])
-  + (data["Single Dad - Children under 18"] - data["Single Dad - Children under 18 - under 5"])
-  + (data["Single Mom - Children under 18"] = data["Single Mom - Children under 18 - under 5"])
+  (data["married_family_children_5_to_17"] + data["single_dad_children_5_to_17"] + data["single_mom_children_5_to_17"])
 
 document.getElementById("total-families-poverty-with-schoolchildren").innerHTML = totalFamiliesWithSchoolchildren;
 
-document.getElementById("single-dad-poverty-with-schoolchildren").innerHTML = data["Single Dad - Children under 18"] - data["Single Dad - Children under 18 - under 5"];
+document.getElementById("two-parent-poverty-with-schoolchildren").innerHTML = data["married_family_children_5_to_17"];
 
+document.getElementById("single-dad-poverty-with-schoolchildren").innerHTML = data["single_dad_children_5_to_17"]
 
-document.getElementById("single-mom-poverty-with-schoolchildren").innerHTML = data["Single Mom - Children under 18"] = data["Single Mom - Children under 18 - under 5"];
+document.getElementById("single-mom-poverty-with-schoolchildren").innerHTML = data["single_mom_children_5_to_17"]
