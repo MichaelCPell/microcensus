0f5df7e6c438aa5eb2a8ff2af766115899b1a8a5
Michael Pell <MichaelCPell@gmail.com>
2017-03-07 10:21:49 -0500

    Wires up data object to JS for graph

--- src/report_templates/long_house_value_report.js
+++ src/report_templates/long_house_value_report.js
@@ -2,32 +2,32 @@ console.log(`Flag One!!`)
 console.log(data)
 
 
-// $(".address").html(data["address"]);
-// if(data.type == "polygon"){
-//   $("#point").hide();
-// }else{
-//   $("#radius").html(data["radius"]);
-//   $("#polygon").hide()
-// }
+$(".address").html(data["address"]);
+if(data.type == "polygon"){
+  $("#point").hide();
+}else{
+  $("#radius").html(data["radius"]);
+  $("#polygon").hide()
+}
 
-// var chart = c3.generate({
-//   bindto: '#chart',
-//   size: {
-//         height: 320,
-//         // width: 350
-//   },
-//   data: {
-//     columns: [
-//       ["Population", data[0][1990], data[1][2000], data[2][2010]]
-//     ]
-//   },
-//   color: {
-//       pattern: ['#D49D2E', '#60A0BE', '#E0601B', '6D3F94']
-//   },
-//   axis: {
-//     x: {
-//       type: 'category',
-//       categories: ['1990', '2000', '2010']
-//     }
-//   }
-// });
+var chart = c3.generate({
+  bindto: '#chart',
+  size: {
+        height: 320,
+        // width: 350
+  },
+  data: {
+    columns: [
+      ["House Value", data["1990"]["median"], data["2000"]["median"], data["2010"]["median"]]
+    ]
+  },
+  color: {
+      pattern: ['#D49D2E', '#60A0BE', '#E0601B', '6D3F94']
+  },
+  axis: {
+    x: {
+      type: 'category',
+      categories: ['1990', '2000', '2010']
+    }
+  }
+});
