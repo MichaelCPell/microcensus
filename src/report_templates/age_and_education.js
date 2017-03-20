b7dc6bb1ba15f37ea6fab7986b9bb5ea9ec48e02
ANDREA DARDON <andardon1111@gmail.com>
2017-03-20 16:06:06 -0600

    Integrating Age and Education report. The Age graph still doesn't work

--- src/report_templates/age_and_education.js
+++ src/report_templates/age_and_education.js
@@ -1,3 +1,81 @@
+window.printReport = function(){
+  chart1.resize({
+    height: 260,
+    width: 400
+  });
+
+  chart2.resize({
+    height: 260,
+    width: 400
+  });
+
+  $("#map").css("height", "180px")
+
+  setTimeout(function(){window.print()}, 1000)
+}
+
+//
+
+// var array = []
+// for(key in data["age"]["male"]){
+//   array.push(data["age"]["male"][key])
+// }
+// array.unshift("male")
+//
+// var array2 = []
+// for(key in data["age"]["female"]){
+//   array2.push(data["age"]["female"][key])
+// }
+// array2.unshift("female")
+//
+//
+// var chart = c3.generate({
+// 	bindto: '#chart1',
+//   data: {
+//     columns: [
+//       array,
+//       array2
+//     ],
+//     type: 'bar',
+//   },
+//
+//   axis: { x: {
+//   	type: 'category',
+//     categories: Object.keys(data["age"]["male"])
+//   }},
+// });
+
+//
+
+
+c3.generate({
+  bindto: '#chart2',
+    data: {
+      json: [data["education"]["sum"]],
+       keys: {
+        value: ['Regular high school diploma', 'GED or alternative credential', 'Some college, less than 1 year', 'Some college, 1 or more years, no degree', 'Associate\'s degree', 'Bachelor\'s degree', 'Master\'s degree', 'Professional school degree', 'Doctorate degree', 'Did Not Finish High School']
+      },
+      type: 'bar'
+      },
+      color: {
+      pattern: ['#E39920', '#53B5B5', '#CD249A', '#F56223', '#6FAAD6','#A33E54', '#27668D','#75AB36', '#6E37B6', '#E79F87']
+      },
+      tooltip: {
+      grouped: false,
+      format: {
+          title: function (d) { return 'Data'},
+          value: function (value, ratio, id) {
+              var format = id === 'data1' ? d3.format(',') : d3.format('');
+              return format(value);
+          }
+    }
+  }
+});
+
+//
+
+//
+
 $(".address").html(data["address"]);
 if(data.type == "polygon"){
   $("#point").hide();
@@ -6,59 +84,4 @@ if(data.type == "polygon"){
   $("#polygon").hide()
 }
 
-c3.generate({
-  bindto: '#chart1',
-    data: {
-        json: [data["education"]["sum"]],
-        keys: {
-            value: ['Regular high school diploma', 'GED or alternative credential', 'Some college, less than 1 year', 'Some college, 1 or more years, no degree', 'Associate\'s degree', 'Bachelor\'s degree', 'Master\'s degree', 'Professional school degree', 'Doctorate degree', 'Did Not Finish High School']
-        },
-        type: 'bar'
-    },
-    color: {
-        pattern: ['#A12821', '#27668D', '#75AB36', '#E39920', 'A33E54','#27668D', '#31B2A1','#F56223', '#53B5B5', '#E79F87']
-      }
-});
-
-c3.generate({
-  bindto: '#chart2',
-    data: {
-        json: [data["age"]["male"]],
-        keys: {
-            value: ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
-        },
-        type: 'bar'
-    },
-    color: {
-        pattern: ['#27668D', '#75AB36', '#E39920', 'A33E54','#27668D', '#31B2A1','#F56223', '#53B5B5', '#E79F87']
-      }
-});
-
-c3.generate({
-  bindto: '#chart3',
-    data: {
-        json: [data["age"]["female"]],
-        keys: {
-            value: ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
-        },
-        type: 'bar'
-    },
-    color: {
-        pattern: ['#27668D', '#75AB36', '#E39920', 'A33E54','#27668D', '#31B2A1','#F56223', '#53B5B5', '#E79F87']
-      }
-});
-
-
-c3.generate({
-  bindto: '#chart4',
-    data: {
-        json: [data["age"]["total"]],
-        keys: {
-            value: ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
-        },
-        type: 'bar'
-    },
-    color: {
-        pattern: ['#27668D', '#75AB36', '#E39920', 'A33E54','#27668D', '#31B2A1','#F56223', '#53B5B5', '#E79F87']
-      }
-});
+//
