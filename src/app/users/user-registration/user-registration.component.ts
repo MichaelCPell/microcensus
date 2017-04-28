fc18e1a8d670880596e67ae6ccf262dfa93be7c7
Michael Pell <MichaelCPell@gmail.com>
2017-04-28 18:36:29 -0400

    a few ux fixes

--- src/app/users/user-registration/user-registration.component.ts
+++ src/app/users/user-registration/user-registration.component.ts
@@ -1,25 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, Inject, Injectable, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 import { UserRegistrationService } from "../cognito.service";
 import { RegistrationUser } from "../registration-user";
 import { CognitoCallback } from "../cognito.service";
 import { User } from "../user"
+import {PageScrollService, PageScrollInstance} from "ng2-page-scroll";
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
     selector: 'awscognito-angular2-app',
     templateUrl: './user-registration.component.html',
     styleUrls: ['./user-registration.component.css']
 })
-export class UserRegistrationComponent implements CognitoCallback {
+export class UserRegistrationComponent implements CognitoCallback, OnInit {
     registrationUser:RegistrationUser;
     router:Router;
+    route:ActivatedRoute;
     errorMessage:string;
 
     constructor(public userRegistration:UserRegistrationService,
                 router:Router,
-                private user:User) {
+                route:ActivatedRoute,
+                private user:User,
+                private pageScrollService: PageScrollService,
+                @Inject(DOCUMENT) private document: any) {
         this.router = router;
+        this.route = route;
         this.onInit();
+    }
+
+    ngOnInit(){
+        this.route.fragment.subscribe((data) => {
+            let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, `#${data}`);
+            this.pageScrollService.start(pageScrollInstance)
+        })
     }
 
     onInit() {
