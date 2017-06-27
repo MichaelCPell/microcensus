e671edc36bb884a88cfff991d65fba965f923ec3
Michael Pell <MichaelCPell@gmail.com>
2017-06-27 16:47:55 -0400

    Begins dynamo service

---
+++ src/app/services/aws.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from "@angular/core";
+import { environment } from '../../environments/environment';
+import * as AWS from 'aws-sdk';
+
+
+@Injectable()
+export class AwsService {
+    public static _REGION = "us-east-1";
+    public static _IDENTITY_POOL_ID = environment.identityPoolId;
+    public static _USER_POOL_ID = environment.userPoolId;
+
+    constructor() {
+        AWS.config.region = "us-east-1"
+    }
+
+    configAWS(token?:string){
+        let logins = {}
+        logins[`cognito-idp.${AwsService._REGION}.amazonaws.com/${AwsService._USER_POOL_ID}`] = token;
+
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: AwsService._IDENTITY_POOL_ID,
+            Logins: logins
+        })
+    }
+}
--- src/app/shared/ddb.service.ts
+++ src/app/shared/ddb.service.ts
@@ -1,19 +1,35 @@
 import {Injectable} from "@angular/core";
 import {User} from "../models/user";
 import {Observable} from "rxjs/Observable";
-
-declare var AWS:any;
-declare var AWSCognito:any;
+import { Store } from "@ngrx/store";
+import * as fromRoot from '../reducers';
+import * as AWS from 'aws-sdk';
+import * as fromUser from '../reducers/users';
 
 @Injectable()
 export class DynamoDBService {
-
-    constructor() {
+    db = new AWS.DynamoDB.DocumentClient();
+    
+    constructor(private store:Store<fromRoot.State>) {
         console.log("DynamoDBService: constructor");
+
+        this.store.select("user")
+          .filter( user => user['current'])
+          .subscribe( (user:fromUser.State) => {
+          console.log(`ddb got the user`)
+          console.log(user)
+          this.db.get({
+            TableName: "users",
+            Key: {email: user.current.email},
+            AttributesToGet: ['locations', 'reportTypes']
+          }, (err, data) => {
+            console.log(err)
+            console.log(data)
+          })
+        })
     }
 
     public addLocation(researchArea, email){
-      var db = new AWS.DynamoDB.DocumentClient();
       var params = {
         TableName: 'users',
         Key: { email: email},
@@ -36,7 +52,7 @@ export class DynamoDBService {
         ReturnValues: 'ALL_NEW'
       }
 
-      db.update(params, ((err, data) => {
+      this.db.update(params, ((err, data) => {
         if(err) console.log(err);
         else{
           // this.user.updateFromDdb(data["Attributes"])
